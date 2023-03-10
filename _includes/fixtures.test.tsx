6e79ed5662c7444693727a484a3cbb0d1ac76e46
pauleveritt <paul.everitt@jetbrains.com>
2023-03-10 08:26:58 -0500

    Bring back the technology test.

--- _includes/fixtures.test.tsx
+++ _includes/fixtures.test.tsx
@@ -34,7 +34,7 @@ test("should NOT have resolved references", () => {
   expect(tip0 && tip0.references).not.to.exist;
 });
 test("should have resolved references", () => {
-  const { tipItems, authorItems, topicItems } = fixtures;
+  const { tipItems, authorItems, topicItems, technologyItems } = fixtures;
   const { resolvedCollections } = fixtures;
   const tip0 = resolvedCollections.allResources.get(tipItems[0].page.url);
   if (tip0 && tip0.references) {
@@ -42,10 +42,9 @@ test("should have resolved references", () => {
     expect(tip0.references).to.exist;
     expect(tip0.references.author.title).to.equal(authorItems[0].data.title);
     expect(tip0.references.topics[0].title).to.equal(topicItems[0].data.title);
-    // TODO Move from label to type:label
-    // expect(tip0.references.technologies[0].title).to.equal(
-    //   technologyItems[0].data.title
-    // );
+    expect(tip0.references.technologies[0].title).to.equal(
+      technologyItems[0].data.title
+    );
   }
 });
 
