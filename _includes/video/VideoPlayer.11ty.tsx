3eebd9a3b71ebc0dc252fee23338d9d1be2497c4
pauleveritt <paul.everitt@jetbrains.com>
2023-03-15 16:53:13 -0400

    Fix the VideoPlayer test.

--- _includes/video/VideoPlayer.11ty.tsx
+++ _includes/video/VideoPlayer.11ty.tsx
@@ -1,20 +1,21 @@
-import h, { JSX } from "vhtml";
+import h, {JSX} from "vhtml";
 
 export type VideoPlayerProps = {
-  source: string;
-  poster?: string;
+    source: string;
+    poster?: string;
 };
 
 // @ts-ignore
 // todo: poster isn't really required because YouTube has thumbnails
-const VideoPlayer = ({ source, poster }: VideoPlayerProps): JSX.Element => {
-  return (
-    <div
-      class="video-player"
-      data-plyr-provider="youtube"
-      data-plyr-embed-id={`${source}?iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1`}
-    ></div>
-  );
+const VideoPlayer = ({source, poster}: VideoPlayerProps): JSX.Element => {
+    return (
+        <div
+            title="Video Player"
+            class="video-player"
+            data-plyr-provider="youtube"
+            data-plyr-embed-id={`${source}?iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1`}
+        ></div>
+    );
 };
 
 export default VideoPlayer;
