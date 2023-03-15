dc228067ebbb36b76f2957fc0d69f68c99190aef
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2023-03-15 14:52:19 -0400

    Switch out VideoJS for Plyr
    Video player has YouTube embed support and is much smaller. We still potentially have issues of too many initial videos on Playlists, but that's a structural issue.

--- _includes/video/VideoPlayer.11ty.tsx
+++ _includes/video/VideoPlayer.11ty.tsx
@@ -5,24 +5,15 @@ export type VideoPlayerProps = {
   poster?: string;
 };
 
+// @ts-ignore
+// todo: poster isn't really required because YouTube has thumbnails
 const VideoPlayer = ({ source, poster }: VideoPlayerProps): JSX.Element => {
   return (
-    <video
-      aria-label="video player"
-      class="video-js vjs-default-skin vjs-16-9 vjs-big-play-centered"
-      controls
-      data-setup='{ "techOrder": ["youtube"], "fluid" : true }'
-      poster={poster}
-    >
-      <source src={source} type="video/youtube" />
-      <p className="vjs-no-js">
-        To view this video please enable JavaScript, and consider upgrading to a
-        web browser that
-        <a href="https://videojs.com/html5-video-support/" target="_blank">
-          supports HTML5 video
-        </a>
-      </p>
-    </video>
+    <div
+      class="video-player"
+      data-plyr-provider="youtube"
+      data-plyr-embed-id={`${source}?iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1`}
+    ></div>
   );
 };
 
