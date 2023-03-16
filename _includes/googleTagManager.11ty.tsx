321b7871fd5720da855d5ccc09124d493bf4870c
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2023-03-16 13:35:59 -0400

    Add Google tag manager script and noscript elements.
    JETBRAINS-GUIDE-T-29

---
+++ _includes/googleTagManager.11ty.tsx
@@ -0,0 +1,32 @@
+import h, { JSX } from "vhtml";
+
+export type GoogleTagManagerProps = {
+  googleTagManagerId: string;
+};
+
+export const GoogleTagManagerHeadScript = ({
+  googleTagManagerId,
+}: GoogleTagManagerProps): JSX.Element => {
+  const script = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','${googleTagManagerId}');`;
+
+  return <script dangerouslySetInnerHTML={{ __html: script }}></script>;
+};
+
+export const GoogleTagManagerBodyNoScript = ({
+  googleTagManagerId,
+}: GoogleTagManagerProps): JSX.Element => {
+  return (
+    <noscript>
+      <iframe
+        src={`https://www.googletagmanager.com/ns.html?id=${googleTagManagerId}`}
+        height="0"
+        width="0"
+        style="display:none;visibility:hidden"
+      ></iframe>
+    </noscript>
+  );
+};
--- _includes/layouts/BaseLayout.11ty.tsx
+++ _includes/layouts/BaseLayout.11ty.tsx
@@ -5,11 +5,19 @@ import Footer from "../footer/Footer.11ty";
 import { LayoutContext, LayoutProps } from "../../src/models";
 import { MetaOpenGraphImage } from "../../src/plugins/metaOpenGraphImagePlugin";
 import { Resource } from "../../src/ResourceModels";
+import {
+  GoogleTagManagerHeadScript,
+  GoogleTagManagerBodyNoScript,
+} from "../googleTagManager.11ty";
 
 export type BaseLayoutProps = {
   children: string[];
   site: any;
   title: string;
+  subtitle: string;
+  longVideo?: string;
+  shortVideo?: string;
+  resourceType: string;
 } & LayoutProps;
 
 export function BaseLayout(
@@ -17,10 +25,20 @@ export function BaseLayout(
   data: BaseLayoutProps
 ): JSX.Element {
   // @ts-ignore
-  const { children, title, subtitle, site, longVideo, shortVideo, resourceType, collections } = data;
+  const {
+    children,
+    title,
+    subtitle,
+    site,
+    longVideo,
+    shortVideo,
+    resourceType,
+    collections,
+  } = data;
   const { siteTitle, copyright, siteUrl } = site;
+
   // TODO This is a hack. Bake it into the contract.
-  const hasVideo = (longVideo || shortVideo) || resourceType == "playlist";
+  const hasVideo = longVideo || shortVideo || resourceType == "playlist";
 
   // determine if there's an og:image
   let cardThumbnail;
@@ -29,39 +47,58 @@ export function BaseLayout(
     cardThumbnail = resource?.cardThumbnail;
   }
 
-  return "<!doctype html>" + (
-    <html lang="en">
-    <head>
-      <meta charset="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>
-        {data.title} - {siteTitle}
-      </title>
-      <link rel="stylesheet" href="/assets/guide.css" />
-      <link rel="icon" href="/assets/favicon.ico" type="image/x-icon" />
-      <link rel="shortcut icon" href="/assets/favicon.ico" />
-      <link rel="alternate" type="application/rss+xml" title={`${title} RSS Feed`} href="/rss.xml" />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={subtitle} />
-      <meta property="og:type" content="article" />
-      <meta property="article:published_time" content="2023-02-17" />
-      <meta property="article:author" content="" />
-      <meta property="article:section" content="" />
-      {cardThumbnail && <meta property="og:image:alt" content={title} />}
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="@jetbrains" />
-      <script defer src="/assets/js/site.js" type="module"></script>
-      {hasVideo &&
-        <script src="/assets/js/video.js" type="module"></script>
-      }
-    </head>
-    <body>
-    <Navbar site={site}></Navbar>
-    {children}
-    <Footer copyright={copyright}></Footer>
-    {cardThumbnail && <MetaOpenGraphImage siteUrl={siteUrl} src={cardThumbnail} />}
-    </body>
-    </html>
+  return (
+    "<!doctype html>" +
+    (
+      <html lang="en">
+        <head>
+          <meta charset="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <title>
+            {data.title} - {siteTitle}
+          </title>
+          <link rel="stylesheet" href="/assets/guide.css" />
+          <link rel="icon" href="/assets/favicon.ico" type="image/x-icon" />
+          <link rel="shortcut icon" href="/assets/favicon.ico" />
+          <link
+            rel="alternate"
+            type="application/rss+xml"
+            title={`${title} RSS Feed`}
+            href="/rss.xml"
+          />
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={subtitle} />
+          <meta property="og:type" content="article" />
+          <meta property="article:published_time" content="2023-02-17" />
+          <meta property="article:author" content="" />
+          <meta property="article:section" content="" />
+          {cardThumbnail && <meta property="og:image:alt" content={title} />}
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="@jetbrains" />
+          <script defer src="/assets/js/site.js" type="module"></script>
+          {hasVideo && (
+            <script src="/assets/js/video.js" type="module"></script>
+          )}
+          <GoogleTagManagerHeadScript
+            googleTagManagerId={site.googleTagManagerId}
+          />
+        </head>
+        <body>
+          <GoogleTagManagerBodyNoScript
+            googleTagManagerId={site.googleTagManagerId}
+          />
+          <Navbar site={site}></Navbar>
+          {children}
+          <Footer copyright={copyright}></Footer>
+          {cardThumbnail && (
+            <MetaOpenGraphImage siteUrl={siteUrl} src={cardThumbnail} />
+          )}
+        </body>
+      </html>
+    )
   );
 }
 
