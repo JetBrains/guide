28873490e8022a69b18ca94beaeaa5051a177c8f
Maarten Balliauw <maarten@maartenballiauw.be>
2023-03-17 08:04:31 +0000

    Add tests to compare site configuration with PyCharm Guide

---
+++ src/infrastructure/configuration.test.ts
@@ -0,0 +1,23 @@
+import { expect, test, describe } from "vitest";
+import fs from "fs";
+import path from "upath";
+
+const sitesPath = "./sites/";
+// @ts-ignore
+const pycharmSiteConfig = JSON.parse(fs.readFileSync(path.join(sitesPath, "pycharm-guide", "_data", "site.json")));
+
+describe.each(
+  fs.readdirSync(`./sites/`)
+)('describe site $site', (site) => {
+  test(`contains site.json and layout.js`, () => {
+    expect(fs.existsSync(path.join(sitesPath, site, "_data", "site.json"))).to.equal(true);
+    expect(fs.existsSync(path.join(sitesPath, site, "_data", "layout.js"))).to.equal(true);
+  })
+
+  test(`has site.json with same (root) configuration keys as PyCharm Guide`, () => {
+    // @ts-ignore
+    const siteConfig = JSON.parse(fs.readFileSync(path.join(sitesPath, site, "_data", "site.json")));
+
+    expect(Object.keys(siteConfig).sort()).toEqual(Object.keys(pycharmSiteConfig).sort());
+  })
+});
