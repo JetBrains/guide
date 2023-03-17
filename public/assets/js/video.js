3b6bc9e16b8a4e6c8436818b39e2505d8fb168d3
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2023-03-17 09:15:02 -0400

    Fix Playlists loading ALL VIDEOS
    This uses an observer to load the videos as you go through the playlist. That way the YouTube images don't block the critical rendering path.

--- public/assets/js/video.js
+++ public/assets/js/video.js
@@ -1,5 +1,23 @@
 import Plyr from "plyr";
 
-const videos = Array.from(document.querySelectorAll(".video-player")).map(
-  (p) => new Plyr(p)
-);
+const onVisible = function (element, callback) {
+  const options = {
+    root: document.documentElement,
+  };
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      callback(entry, observer);
+    });
+  }, options);
+  observer.observe(element);
+};
+
+const videos = Array.from(document.querySelectorAll(".video-player"));
+videos.forEach((video) => {
+  onVisible(video, (entry, observer) => {
+    if (entry.intersectionRatio > 0) {
+      const plyr = new Plyr(entry.target);
+      observer.unobserve(entry.target);
+    }
+  });
+});
