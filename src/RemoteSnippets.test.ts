5536bfe04496ea14101b842f46f532b2ff9eb6c8
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2023-03-21 11:54:28 -0400

    Support more files and double-sided comments like are found in html and cshtml. Indent from the first comment is respected.

--- src/RemoteSnippets.test.ts
+++ src/RemoteSnippets.test.ts
@@ -1,11 +1,55 @@
 import {expect, test} from "vitest";
-import {parse} from "./RemoteSnippets";
+import { getSnippetByKey, remoteUrlParse } from "./RemoteSnippets";
 
 test("can parse remote resource with block", async () => {
-  const result = await parse("https://gist.githubusercontent.com/khalidabuhakmeh/7dc8c3ef3bb23ef2b7a597ad4d5f694e/raw/2967b12aa2f66ac7d3e89d7e410913b551c9b6ed/test.cs", "Hello World")
+  const result = await remoteUrlParse("https://gist.githubusercontent.com/khalidabuhakmeh/7dc8c3ef3bb23ef2b7a597ad4d5f694e/raw/2967b12aa2f66ac7d3e89d7e410913b551c9b6ed/test.cs", "Hello World")
   const code = result.trim()
   expect(code).to.contains(`public void HelloWorld()
 {
     return "Hello, World!"
 }`);
+});
+
+test("can parse an in-memory code block", () => {
+  // this text has indents
+  const text = `
+            public string Name => "Khalid";
+          
+            // snippet: Hello World
+            public void HelloWorld()
+            {
+                return "Hello, World!"
+            }
+            // end snippet
+`;
+
+  const code = getSnippetByKey(text, "Hello World", ".cs");
+  expect(code).to.contains(`public void HelloWorld()
+{
+    return "Hello, World!"
+}`);
+})
+
+test("can parse an in-memory cshtml code block", () => {
+  // this text has indents
+  const text = `
+            @* snippet: Cool Beans *@
+            <h1>@Model.Khalid Says So!</h1>
+            @* end snippet *@
+`;
+
+  const code = getSnippetByKey(text, "Cool Beans", ".cshtml");
+  expect(code).to.contains(`<h1>@Model.Khalid Says So!</h1>`);
+})
+
+test("can parse an in-memory html code block", () => {
+  // this text has indents
+  const text = `
+            <!-- snippet: TikTok Maarten -->
+            <h1>Maarten ❤️ Dance!</h1>
+            <!-- end snippet -->
+`;
+
+  const code = getSnippetByKey(text, "TikTok Maarten", ".html");
+  expect(code).to.contains(`<h1>Maarten ❤️ Dance!</h1>`);
 })
--- src/RemoteSnippets.ts
+++ src/RemoteSnippets.ts
@@ -1,40 +1,90 @@
 import path from "upath";
 // @ts-ignore
-import EleventyFetch from "@11ty/eleventy-fetch"
+import EleventyFetch from "@11ty/eleventy-fetch";
+import fs from "fs/promises";
 
-let commentStylePicker = new Map<string, string>([
-  [".cs", "//"],
-  [".py", "#"]
+type CommentInformation = {
+  start: string,
+  remove: string[],
+  keyword: string
+}
+
+let commentStylePicker = new Map<string, CommentInformation>([
+  [".cs", { start: "//", keyword: "snippet", remove: ["//"] }],
+  [".py", { start: "#", keyword: "snippet", remove: ["#"] }],
+  [".js", { start: "//", keyword: "snippet", remove: ["//"] }],
+  [".java", { start: "//", keyword: "snippet", remove: ["//"] }],
+  [".cshtml", { start: "@*", keyword: "snippet", remove: ["@*", "*@"] }],
+  [".html", { start: "<!--", keyword: "snippet", remove: ["<!--", "-->"] }]
 ]);
 
-export async function parse(remoteUrl: string, key: string) : Promise<string> {
-  const comment = commentStylePicker.get(path.extname(remoteUrl));
-  const text = await EleventyFetch(remoteUrl, { duration: "1d", type: "text"});
+export async function remoteUrlParse(remoteUrl: string, key: string): Promise<string> {
+  const text = await EleventyFetch(remoteUrl, { duration: "1d", type: "text" });
+  return getSnippetByKey(text, key, path.extname(remoteUrl));
+}
+
+export async function localFileParse(filePath: string, key: string): Promise<string> {
+  const text = await fs.readFile(filePath, "utf-8");
+  const extension = path.extname(filePath);
+
+  return getSnippetByKey(text, key, extension);
+}
+
+export function getSnippetByKey(text: string, key: string, extension: string): string {
+  const map = getAllSnippets(text, extension);
+  return map.get(key) ?? "";
+}
+
+export function getAllSnippets(text: string, extension: string): Map<string, string> {
+  const comment = commentStylePicker.get(extension);
+
+  if (!comment) {
+    throw new Error(`unknown file extension of "${extension}"`);
+  }
+
   const lines = text.split("\n");
 
   const map = new Map<string, string>();
-  let name = null;
-  let codeBlock = '';
+  let name: any = null;
+  let codeBlock = "";
+  let indent = 0;
 
   for (const index in lines) {
-    const line = lines[index];
-    if (line.toLowerCase().startsWith(`${comment} snippet:`)) {
-      name = line.replace(`${comment} snippet:`, "").trim()
-      // new block
-      codeBlock = '';
+    const currentLine = lines[index];
+    const check = currentLine.toLowerCase().trim();
+
+    if (check.startsWith(`${comment.start} ${comment.keyword}:`)) {
+      // get current casing of snippet name
+      name = currentLine.replace(`${comment.keyword}:`, "");
+      comment.remove.forEach(v => name = name.replace(v, ""));
+      name = name.trim();
+
+      if (name.length > 0) {
+        // new block
+        codeBlock = "";
+        // difference between currentLine length and trim
+        indent = currentLine.length - currentLine.trimStart().length;
+      }
+
       continue;
     }
-    if (line.toLowerCase().startsWith(`${comment} end snippet`)) {
+
+    if (check.startsWith(`${comment.start} end ${comment.keyword}`)) {
       if (name) {
         map.set(name, codeBlock);
       }
-      codeBlock = '';
+      codeBlock = "";
       name = null;
+      indent = 0;
       continue;
     }
-    codeBlock += line + "\n";
-  }
 
-  return map.get(key) ?? '';
+    // Only build code block when a name exists
+    if (name) {
+      const clean = `${currentLine.substring(indent)}\n`;
+      codeBlock += clean;
+    }
+  }
+  return map;
 }
 
