aa837a548159140901a65bcd68ffff6beee803f3
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2023-03-29 14:05:25 -0400

    Add Ability To Start and Stop videos on seconds
    This is in relation to JETBRAINS-GUIDE-T-54

--- _includes/video/VideoPlayer.11ty.tsx
+++ _includes/video/VideoPlayer.11ty.tsx
@@ -1,21 +1,30 @@
-import h, {JSX} from "vhtml";
+import h, { JSX } from "vhtml";
 
 export type VideoPlayerProps = {
-    source: string;
-    poster?: string;
+  source: string;
+  poster?: string;
+  start?: number;
+  end?: number;
 };
 
 // @ts-ignore
 // todo: poster isn't really required because YouTube has thumbnails
-const VideoPlayer = ({source, poster}: VideoPlayerProps): JSX.Element => {
-    return (
-        <div
-            title="Video Player"
-            class="video-player"
-            data-plyr-provider="youtube"
-            data-plyr-embed-id={`${source}?iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1`}
-        ></div>
-    );
+const VideoPlayer = ({
+  source,
+  start,
+  end,
+  poster,
+}: VideoPlayerProps): JSX.Element => {
+  return (
+    <div
+      title="Video Player"
+      class="video-player"
+      data-plyr-start={start}
+      data-plyr-end={end}
+      data-plyr-provider="youtube"
+      data-plyr-embed-id={`${source}?iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1`}
+    ></div>
+  );
 };
 
 export default VideoPlayer;
