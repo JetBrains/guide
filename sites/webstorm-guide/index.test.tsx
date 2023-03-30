c6aee08665bffa2d9bb1db74a480bfa27f42e699
pauleveritt <paul.everitt@jetbrains.com>
2023-03-30 15:14:58 -0400

    Fix home page tests.

--- sites/webstorm-guide/index.test.tsx
+++ sites/webstorm-guide/index.test.tsx
@@ -23,5 +23,5 @@ test("should render WebStormHomepage", () => {
   const homepage = new WebStormHomepage();
   const render = homepage.render;
   document.body.innerHTML = render.call(context, pageLayoutData);
-  expect(screen.getAllByRole("link", { name: "Resource" }).length).to.equal(9);
+  expect(screen.getByText("Learn Something New Quickly")).to.exist;
 });
