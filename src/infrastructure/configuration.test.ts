6d7aa520aae86f576047348d345349d22a97141f
pauleveritt <paul.everitt@jetbrains.com>
2023-06-16 11:56:20 -0600

    Remove -guide from all sites.

--- src/infrastructure/configuration.test.ts
+++ src/infrastructure/configuration.test.ts
@@ -4,20 +4,28 @@ import path from "upath";
 
 const sitesPath = "./sites/";
 // @ts-ignore
-const pycharmSiteConfig = JSON.parse(fs.readFileSync(path.join(sitesPath, "pycharm-guide", "_data", "site.json")));
+const pycharmSiteConfig = JSON.parse(
+  fs.readFileSync(path.join(sitesPath, "pycharm", "_data", "site.json"))
+);
 
-describe.each(
-  fs.readdirSync(`./sites/`)
-)('describe site $site', (site) => {
+describe.each(fs.readdirSync(`./sites/`))("describe site $site", (site) => {
   test(`contains site.json and layout.js`, () => {
-    expect(fs.existsSync(path.join(sitesPath, site, "_data", "site.json"))).to.equal(true);
-    expect(fs.existsSync(path.join(sitesPath, site, "_data", "layout.js"))).to.equal(true);
-  })
+    expect(
+      fs.existsSync(path.join(sitesPath, site, "_data", "site.json"))
+    ).to.equal(true);
+    expect(
+      fs.existsSync(path.join(sitesPath, site, "_data", "layout.js"))
+    ).to.equal(true);
+  });
 
   test(`has site.json with same (root) configuration keys as PyCharm Guide`, () => {
     // @ts-ignore
-    const siteConfig = JSON.parse(fs.readFileSync(path.join(sitesPath, site, "_data", "site.json")));
+    const siteConfig = JSON.parse(
+      fs.readFileSync(path.join(sitesPath, site, "_data", "site.json"))
+    );
 
-    expect(Object.keys(siteConfig).sort()).toEqual(Object.keys(pycharmSiteConfig).sort());
-  })
+    expect(Object.keys(siteConfig).sort()).toEqual(
+      Object.keys(pycharmSiteConfig).sort()
+    );
+  });
 });
