5731df319f7b552896f0fe61f912f1320ac725b4
mukulmantosh <mukul.mantosh@jetbrains.com>
2023-06-26 13:28:14 +0530

    Feat: Cloudfront Redirect

---
+++ cdn.js
@@ -0,0 +1,41 @@
+/* This function is still under development */
+function handler(event) {
+    console.log(helloWorld());
+    var request = event.request;
+    var headers = request.headers;
+    var uri = event["request"]["uri"]
+    var BASE_URL = "https://d11nedkd3onyk7.cloudfront.net/jetbrains/guide/"
+    var JB_PRODUCTS = {
+        "pycharm": "pycharm",
+        "goland": "goland",
+        "webstorm": "webstorm"
+    }
+
+    var data = uri.split("/")
+    data = data.filter((str) => str !== '');
+
+    var product_name = data[0].toLowerCase();
+    if (data.length > 0) {
+        if (JB_PRODUCTS[product_name]) {
+            var new_formed_url = data.slice(2);
+            new_formed_url = new_formed_url.join("/");
+            var final_url = BASE_URL + JB_PRODUCTS[product_name] + "/" + new_formed_url;
+            var response = {
+                statusCode: 301,
+                statusDescription: 'Found',
+                headers: {
+                    "location": {
+                        "value": final_url
+                    }
+                }
+            }
+            return response;
+
+        } else {
+            return request;
+        }
+    } else {
+        return request;
+    }
+
+}
