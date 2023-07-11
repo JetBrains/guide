b2845c97ccadb452b600993832e7b86c870f7317
pauleveritt <paul.everitt@jetbrains.com>
2023-07-11 18:02:45 +0200

    Remove stray console log.

--- public/assets/js/layout.js
+++ public/assets/js/layout.js
@@ -1,14 +1,21 @@
 window.addEventListener("load", function (event) {
-    const referenceDate = new Date();
-    referenceDate.setDate(referenceDate.getDate() - 30);
-    
-    document.querySelectorAll('time.bio-common-card-published')
-        .forEach(element => {
-            const resourceDate = new Date(element.attributes["datetime"].value);
-            console.log(resourceDate);
-            if (resourceDate > referenceDate) {
-                element.closest(".bio-resourcecard").classList.add("has-background-info-light");
-                element.closest(".content").insertAdjacentHTML("afterbegin", '<span class="tag is-warning is-pulled-right">New</span>');
-            }
-        });
+  const referenceDate = new Date();
+  referenceDate.setDate(referenceDate.getDate() - 30);
+
+  document
+    .querySelectorAll("time.bio-common-card-published")
+    .forEach((element) => {
+      const resourceDate = new Date(element.attributes["datetime"].value);
+      if (resourceDate > referenceDate) {
+        element
+          .closest(".bio-resourcecard")
+          .classList.add("has-background-info-light");
+        element
+          .closest(".content")
+          .insertAdjacentHTML(
+            "afterbegin",
+            '<span class="tag is-warning is-pulled-right">New</span>'
+          );
+      }
+    });
 });
