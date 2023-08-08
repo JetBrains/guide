9e95db83b7fbdc5d7cf85f738e0574872dfd701b
pauleveritt <paul.everitt@jetbrains.com>
2023-08-07 20:16:27 -0400

    Wire up TutorialsLayout.

--- _includes/resources/tutorial/TutorialsLayout.11ty.tsx
+++ _includes/resources/tutorial/TutorialsLayout.11ty.tsx
@@ -5,23 +5,42 @@ import {
 } from "../../layouts/ReferenceLayout.11y";
 import { LayoutContext } from "../../../src/models";
 import ResourceCard from "../../resourcecard/ResourceCard.11ty";
+import { Tutorial } from "./TutorialModels";
 
-export function TutorialsLayout(
-  this: LayoutContext,
-  data: ReferenceLayoutProps
-): JSX.Element {
-  const tutorials = this.getResources({
-    resourceType: "tutorial",
-  });
-  const figure = undefined;
-  const listing = (
-    <>
-      {tutorials.map((tutorial) => {
-        return <ResourceCard resource={tutorial}></ResourceCard>;
-      })}
-    </>
-  );
-  return <ReferenceLayout {...data} figure={figure} listing={[listing]} />;
+export class TutorialsLayout {
+  data() {
+    return {
+      eleventyExcludeFromCollections: true,
+      title: "Tutorials",
+      subtitle:
+        "Learn how to work with technologies using detailed, multi-step lessons accompanied by code samples, instructions, and videos.",
+    };
+  }
+
+  render(this: LayoutContext, data: ReferenceLayoutProps): JSX.Element {
+    const { content, pagination } = data;
+    const paginationItems = pagination ? pagination.items : [];
+    const tutorials: Tutorial[] = paginationItems.map((t: any) => {
+      return this.getResource(t.url) as Tutorial;
+    });
+
+    const figure = undefined;
+    const listing = (
+      <>
+        {tutorials.map((tutorial) => {
+          return <ResourceCard resource={tutorial}></ResourceCard>;
+        })}
+      </>
+    );
+    return (
+      <ReferenceLayout
+        {...data}
+        figure={figure}
+        listing={[listing]}
+        content={content}
+      />
+    );
+  }
 }
 
-export const render = TutorialsLayout;
+module.exports = TutorialsLayout;
