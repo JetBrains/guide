e5eb43efe1325da1173002c58746e948164feb4f
pauleveritt <paul.everitt@jetbrains.com>
2023-08-08 14:56:36 -0400

    Add the All button.

--- _includes/sectionlisting/SectionListing.11ty.tsx
+++ _includes/sectionlisting/SectionListing.11ty.tsx
@@ -16,30 +16,28 @@ function SectionListing({
   resources,
 }: SectionListingProps) {
   /* A reusable component for section-style paginated ResourceCard listings */
+  const listing = (
+    <div class="columns is-multiline">
+      {resources.map((resource) => {
+        return <ResourceCard resource={resource}></ResourceCard>;
+      })}
+    </div>
+  );
+
   return (
     <section class="section">
-      <div class="container">
-        <div class="is-flex">
-          <div>
-            <h2 class="mt-2 mb-4 is-size-1 has-text-weight-bold">{title}</h2>
-            {subtitle && <p class="subtitle has-text-grey mb-5">{subtitle}</p>}
-          </div>
+      <div class="columns is-vcentered is-mobile">
+        <div class="column is-8">
+          <h2 class="mt-2 mb-4 is-size-1 has-text-weight-bold">{title}</h2>
+          {subtitle && <p class="subtitle has-text-grey mb-5">{subtitle}</p>}
         </div>
-        <div class="is-flex">
-          {resources.map((resource) => {
-            return <ResourceCard resource={resource}></ResourceCard>;
-          })}
-        </div>
-        {moreLink && (
-          <a
-            class="button is-rounded"
-            href={moreLink}
-            data-config-id="primary-action"
-          >
-            Learn More
+        <div class="column has-text-right">
+          <a class="button is-rounded is-outlined" href={moreLink}>
+            All...
           </a>
-        )}
+        </div>
       </div>
+      <div class="container">{listing}</div>
     </section>
   );
 }
