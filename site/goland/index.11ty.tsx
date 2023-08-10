31ede3e5613308ef9fc0146233ace1e0f77d6fd2
pauleveritt <paul.everitt@jetbrains.com>
2023-08-10 08:13:44 -0400

    Fix link spacing.

--- site/goland/index.11ty.tsx
+++ site/goland/index.11ty.tsx
@@ -51,51 +51,49 @@ export class GoLandHomepage {
           <div class="container">
             <div class="columns">
               <div class="column is-three-quarters-desktop">
-                <div class="bd-content content">
-                  <div class="content">
-                    <h2>Learn something new, quickly</h2>
+                <div class="content">
+                  <h2>Learn something new, quickly</h2>
 
-                    <p>
-                      JetBrains tools like
-                      <a href="https://jetbrains.com/go/">GoLand</a> are
-                      powerful developer productivity tools. What is the best
-                      way to learn how to harness that power?
-                    </p>
-                    <p>
-                      You can find useful information on our Twitter account,
-                      <a href="https://twitter.com/GoLandIDE">@GoLandIDE</a>, or
-                      our
-                      <a href="https://blog.jetbrains.com/go/">product blog</a>.
-                      Plus, the
-                      <a href="https://www.jetbrains.com/go/learn/">
-                        documentation
-                      </a>
-                      is always there to help. However, wouldn't it be better if
-                      you had everything you needed to learn in one place?
-                    </p>
+                  <p>
+                    JetBrains tools like{" "}
+                    <a href="https://jetbrains.com/go/">GoLand</a> are powerful
+                    developer productivity tools. What is the best way to learn
+                    how to harness that power?
+                  </p>
+                  <p>
+                    You can find useful information on our Twitter account,{" "}
+                    <a href="https://twitter.com/GoLandIDE">@GoLandIDE</a> , or
+                    our{" "}
+                    <a href="https://blog.jetbrains.com/go/">product blog</a>.
+                    Plus, the{" "}
+                    <a href="https://www.jetbrains.com/go/learn/">
+                      documentation
+                    </a>{" "}
+                    is always there to help. However, wouldn't it be better if
+                    you had everything you needed to learn in one place?
+                  </p>
 
-                    <p>
-                      We have created the GoLand Guide, a collection of
-                      bite-sized visual resources, organized to help spark your
-                      learning. We hope it helps you get into the flow and excel
-                      at what you do.
-                    </p>
+                  <p>
+                    We have created the GoLand Guide, a collection of bite-sized
+                    visual resources, organized to help spark your learning. We
+                    hope it helps you get into the flow and excel at what you
+                    do.
+                  </p>
 
-                    <h2>Sharing Feedback and Contributing</h2>
-                    <p>
-                      The GoLand Guide is also an open project, with
-                      <a href="https://github.com/jetbrains/guide">
-                        a repository in GitHub
-                      </a>
-                      that hosts all the content. We write all the content in
-                      Markdown and render a static site. If you'd like to
-                      contribute to it, please refer to the
-                      <a href="https://github.com/jetbrains/guide/blob/master/README.md">
-                        README
-                      </a>
-                      &nbsp;for more information.
-                    </p>
-                  </div>
+                  <h2>Sharing Feedback and Contributing</h2>
+                  <p>
+                    The GoLand Guide is also an open project, with{" "}
+                    <a href="https://github.com/jetbrains/guide">
+                      a repository in GitHub
+                    </a>{" "}
+                    that hosts all the content. We write all the content in
+                    Markdown and render a static site. If you'd like to
+                    contribute to it, please refer to the{" "}
+                    <a href="https://github.com/jetbrains/guide/blob/master/README.md">
+                      README
+                    </a>{" "}
+                    for more information.
+                  </p>
                 </div>
               </div>
             </div>
--- site/intellij/index.11ty.tsx
+++ site/intellij/index.11ty.tsx
@@ -58,52 +58,43 @@ export class IntelliJHomepage {
           <div class="container">
             <div class="columns">
               <div class="column is-three-quarters-desktop">
-                <div class="bd-content content">
-                  <div class="content">
-                    <p>
-                      JetBrains tools like IntelliJ IDEA are powerful developer
-                      productivity tools. What is the best way to learn how to
-                      harness that power?
-                    </p>
+                <div class="content">
+                  <p>
+                    JetBrains tools like IntelliJ IDEA are powerful developer
+                    productivity tools. What is the best way to learn how to
+                    harness that power?
+                  </p>
 
-                    <p>
-                      You can find useful information on our
-                      <a href="https://twitter.com/intellijidea">
-                        {" "}
-                        Twitter page
-                      </a>
-                      , or our
-                      <a href="https://blog.jetbrains.com/idea/">
-                        product blog
-                      </a>
-                      . Plus, the
-                      <a href="https://www.jetbrains.com/idea/">
-                        documentation
-                      </a>
-                      is always there to help.
-                    </p>
+                  <p>
+                    You can find useful information on our{" "}
+                    <a href="https://twitter.com/intellijidea"> Twitter page</a>
+                    , or our{" "}
+                    <a href="https://blog.jetbrains.com/idea/">product blog</a>.
+                    Plus, the{" "}
+                    <a href="https://www.jetbrains.com/idea/">documentation</a>{" "}
+                    is always there to help.
+                  </p>
 
-                    <p>
-                      We have also created the IntelliJ IDEA Guide, a collection
-                      of bite-sized visual resources, organized to help spark
-                      your learning. We hope it helps you get into the flow and
-                      excel at what you do.
-                    </p>
-                    <h2>Sharing Feedback and Contributing</h2>
-                    <p>
-                      The IntelliJ IDEA Guide is also an open project, with
-                      <a href="https://github.com/jetbrains/guide">
-                        a repository in GitHub
-                      </a>
-                      that hosts all the content. We write all the content in
-                      Markdown and render a static site. If you'd like to
-                      contribute to it, please refer to the
-                      <a href="https://github.com/jetbrains/guide/blob/master/README.md">
-                        README
-                      </a>
-                      for more information.
-                    </p>
-                  </div>
+                  <p>
+                    We have also created the IntelliJ IDEA Guide, a collection
+                    of bite-sized visual resources, organized to help spark your
+                    learning. We hope it helps you get into the flow and excel
+                    at what you do.
+                  </p>
+                  <h2>Sharing Feedback and Contributing</h2>
+                  <p>
+                    The IntelliJ IDEA Guide is also an open project, with{" "}
+                    <a href="https://github.com/jetbrains/guide">
+                      a repository in GitHub
+                    </a>{" "}
+                    that hosts all the content. We write all the content in
+                    Markdown and render a static site. If you'd like to
+                    contribute to it, please refer to the{" "}
+                    <a href="https://github.com/jetbrains/guide/blob/master/README.md">
+                      README
+                    </a>{" "}
+                    for more information.
+                  </p>
                 </div>
               </div>
             </div>
