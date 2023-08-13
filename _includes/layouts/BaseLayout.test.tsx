de11086ce41e18439a141e3c2ad3da7e83fcb838
pauleveritt <paul.everitt@jetbrains.com>
2023-08-13 11:24:30 -0400

    The schema changed but we have to keep the old values around for migration.

--- _includes/layouts/BaseLayout.test.tsx
+++ _includes/layouts/BaseLayout.test.tsx
@@ -1,17 +1,27 @@
-import {expect, test} from "vitest";
+import { expect, test } from "vitest";
 
-import {BaseLayout, BaseLayoutProps} from "./BaseLayout.11ty";
-import fixtures, {baseRenderData} from "../fixtures";
+import { BaseLayout, BaseLayoutProps } from "./BaseLayout.11ty";
+import fixtures, { baseRenderData } from "../fixtures";
 
-const tip0 = fixtures.tipItems[0];
-const renderProps: BaseLayoutProps = {
-  ...baseRenderData,
-  ...tip0.data,
-  page: tip0.page,
-  children: [],
-};
-
-test("BaseLayout", () => {
+test("BaseLayout for Tip", () => {
+  const tip0 = fixtures.tipItems[0];
+  const renderProps: BaseLayoutProps = {
+    ...baseRenderData,
+    ...tip0.data,
+    page: tip0.page,
+    children: [],
+  };
+  document.body.innerHTML = BaseLayout.call(fixtures.context, renderProps);
+  expect(document.body).to.exist;
+});
+test("BaseLayout for Channel", () => {
+  const tip0 = fixtures.tipItems[0];
+  const renderProps: BaseLayoutProps = {
+    ...baseRenderData,
+    ...tip0.data,
+    page: tip0.page,
+    children: [],
+  };
   document.body.innerHTML = BaseLayout.call(fixtures.context, renderProps);
   expect(document.body).to.exist;
 });
