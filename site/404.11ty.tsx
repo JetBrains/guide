e2fa2d39cfdfd5bdd17dd05c0b6ca19530d80ec9
Maarten Balliauw <maarten@maartenballiauw.be>
2023-08-21 17:11:47 +0200

    Test TeamCity

--- site/404.11ty.tsx
+++ site/404.11ty.tsx
@@ -10,7 +10,7 @@ type Http404PageProps = LayoutProps & PageFrontmatter;
 class Http404Page {
   data() {
     return {
-      title: "404 - Page Not Found",
+      title: "404 - Page Not Found NOPE",
       subtitle: "Uh-oh! The page could not be found",
       layout: "",
       eleventyExcludeFromCollections: true,
