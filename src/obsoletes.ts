34d794ac720d013d717d65302364f4f3d1b85b8c
Mukul Mantosh <mukul.mantosh@jetbrains.com>
2023-08-24 17:12:28 +0530

    - Redirect Changes

--- src/obsoletes.ts
+++ src/obsoletes.ts
@@ -5,33 +5,50 @@ import { ReferenceCollection, ResourceCollection } from "./ResourceModels";
 import * as fs from "fs";
 
 export type Obsoletes = {
-  [key: string]: string[];
+	[key: string]: string[];
 };
 
 export function getObsoletes(
-  allReferences: ReferenceCollection,
-  allResources: ResourceCollection
-): Obsoletes {
-  const obsoletes: Obsoletes = {};
-  allReferences.forEach((reference) => {
-    if (reference.obsoletes && reference.obsoletes.length) {
-      obsoletes[reference.url] = reference.obsoletes;
-    }
-  });
-  allResources.forEach((resource) => {
-    if (resource.obsoletes && resource.obsoletes.length) {
-      obsoletes[resource.url] = resource.obsoletes;
-    }
-  });
-  return obsoletes;
+	allReferences: ReferenceCollection,
+	allResources: ResourceCollection
+): string[] {
+	const obsoletes: Obsoletes = {};
+	let nginxRulesList: string[] = [];
+	allReferences.forEach((reference) => {
+		if (reference.obsoletes && reference.obsoletes.length) {
+			obsoletes[reference.url] = reference.obsoletes;
+			let redirectTo = reference.url.replace(/\/+$/, "");
+
+			reference.obsoletes.forEach((data) => {
+				let redirectFrom: string = data.replace(/\/+$/, "");
+				let nginxRule: string =
+					"rewrite ^/guide" +
+					redirectFrom +
+					"(/?.*)$ /guide" +
+					redirectTo +
+					"$1 permanent;";
+				nginxRulesList.push(nginxRule);
+			});
+		}
+	});
+	allResources.forEach((resource) => {
+		if (resource.obsoletes && resource.obsoletes.length) {
+			obsoletes[resource.url] = resource.obsoletes;
+		}
+	});
+	return nginxRulesList;
 }
 
 export function dumpObsoletes(
-  allReferences: ReferenceCollection,
-  allResources: ResourceCollection
+	allReferences: ReferenceCollection,
+	allResources: ResourceCollection
 ): void {
-  const obsoletes = getObsoletes(allReferences, allResources);
-  const content = JSON.stringify(obsoletes);
-  const target = `${__dirname}/../public/obsoletes.json`;
-  fs.writeFileSync(target, content, { flag: "w+" });
+	let finalContent: string = "";
+	const obsoletes = getObsoletes(allReferences, allResources);
+	//const content = JSON.stringify(obsoletes);
+	obsoletes.forEach(function (e) {
+		finalContent += e + "\n";
+	});
+	const target = `${__dirname}/../public/redirect.conf`;
+	fs.writeFileSync(target, finalContent, { flag: "w+" });
 }
