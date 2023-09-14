2e727d88517682180ff7d58e32429f01d5041c99
Maarten Balliauw <maarten.balliauw@jetbrains.com>
2023-09-14 12:44:24 +0000

    JETBRAINS-GUIDE-T-135 Home page as learn page

--- site/index.11ty.tsx
+++ site/index.11ty.tsx
@@ -3,9 +3,19 @@ import h, { JSX } from "vhtml";
 import { LayoutContext, LayoutProps } from "../src/models";
 import { PageFrontmatter } from "../_includes/resources/page/PageModels";
 import { BaseLayout } from "../_includes/layouts/BaseLayout.11ty";
-import FeaturedResource from "../_includes/pageelements/FeaturedResource.11ty";
 import MultiColumnSection from "../_includes/pageelements/MultiColumnSection";
 import ListingSection from "../_includes/pageelements/ListingSection.11ty";
+import ResourceCard from "../_includes/resourcecard/ResourceCard.11ty";
+import { Topic } from "../_includes/resources/topic/TopicModels";
+import {
+	ARTICLE_RESOURCE,
+	LINK_RESOURCE,
+	PLAYLIST_RESOURCE,
+	TIP_RESOURCE,
+	TOPIC_RESOURCE,
+	TUTORIAL_RESOURCE,
+	TUTORIAL_STEP_RESOURCE,
+} from "../src/resourceType";
 
 type IndexPageProps = LayoutProps & PageFrontmatter;
 
@@ -20,20 +30,50 @@ class IndexPage {
 	}
 
 	render(this: LayoutContext, data: IndexPageProps): JSX.Element {
-		const featuredResource = this.getResource(
-			"/idea/tutorials/gitlab-merge-requests/"
-		);
-		const latestContent = this.getResources({
-			resourceTypes: ["tip", "tutorial", "playlist"],
-			limit: 12,
-		});
+		const featuredChannel = this.getResource("/remote/");
+		const channels = [
+			// TODO: These need updating if/when we rename channels to be technology focused.
+			this.getResource("/dotnet/"),
+			this.getResource("/goland/"),
+			this.getResource("/idea/"),
+			this.getResource("/pycharm/"),
+			this.getResource("/webstorm/"),
+		];
+		const communities = [
+			// TODO: These need updating if/when we rename channels to be technology focused.
+			this.getResource("/gamedev/"),
+			this.getResource("/topics/django/"), // TODO: topics have an icon or logo or accent, ResourceCard does not account for this and will not render a nice image). Ideally we standardize on thumbnail for everything.
+			this.getResource("/topics/kotlin/"),
+		];
+
+		const topicNamesForNewestResources = this.getResources({
+			resourceTypes: [
+				TIP_RESOURCE,
+				ARTICLE_RESOURCE,
+				TUTORIAL_RESOURCE,
+				TUTORIAL_STEP_RESOURCE,
+				PLAYLIST_RESOURCE,
+				LINK_RESOURCE,
+			],
+			limit: 60,
+		})
+			.flatMap((r) => r.topics)
+			.slice(0, 20) as string[];
+
+		const topicsForNewestResources = this.getResources({
+			resourceTypes: [TOPIC_RESOURCE],
+			customFilter: (r) =>
+				topicNamesForNewestResources.includes(r.slug) &&
+				(!r.topicType || r.topicType != "product"),
+			limit: 6,
+		}) as Topic[];
 
 		return (
 			<BaseLayout {...data}>
 				<section class="section has-gradient-purple">
 					<div class="container">
 						<div class="columns is-multiline">
-							<div class="column is-8">
+							<div class="column is-7">
 								<h1 class="mt-2 mb-4 is-size-1 has-text-weight-bold has-text-white">
 									Welcome to the JetBrains Guide
 								</h1>
@@ -42,27 +82,110 @@ class IndexPage {
 									have tips, tutorials, videos, articles and much, much more!
 								</p>
 							</div>
+							<ResourceCard
+								columnClassName={"is-3 is-offset-2 has-shadow"}
+								compactMode={true}
+								hasShadow={true}
+								resource={featuredChannel}
+							/>
 						</div>
 					</div>
 				</section>
-				<FeaturedResource resource={featuredResource}>
-					<p>
-						You're working hard on a branch. Then, a colleague finishes some
-						work. Congratulations -- you have a GitLab merge request. How do you
-						productively, efficiently process it with the least distraction?
-					</p>{" "}
-					<p>
-						The IDE can help! JetBrains IDEs have GitLab integration. Learn how
-						to process that merge request, in the IDE, and quickly get back in
-						the flow.
-					</p>
-				</FeaturedResource>
+				{/* TODO PWE: Copied from dotnet/index.11ty.tsx with adaptations:
+				 * thumbnail property is used for the image here vs. cardThumbnail in the dotnet index page
+				 * CSS classnames for the column are different on this page vs. dotnet index page*/}
+				<section class="section has-background-dark">
+					<div class="container">
+						<div class="columns is-multiline is-centered">
+							{channels.map((channel) => {
+								const figure = (
+									<img src={channel.thumbnail} alt={channel.title} />
+								);
 
-				<ListingSection
-					title="Latest"
-					resources={latestContent}
-					moreLink="/latest/"
-				/>
+								return (
+									<div class="column has-background-light m-3 is-6 is-2-desktop py-5 has-box-hover has-text-centered has-position-relative">
+										<a
+											href={channel.url}
+											aria-label={`Topic`}
+											class="is-size-5 has-text-weight-bold title is-stretched-link"
+										>
+											<figure class="image is-48x48 mb-1 mx-auto">
+												{figure}
+											</figure>
+											{channel.title}
+										</a>
+									</div>
+								);
+							})}
+						</div>
+					</div>
+				</section>
+
+				<ListingSection title={"Communities"} resources={communities} />
+
+				<section class="container">
+					<hr />
+				</section>
+
+				<section class="section">
+					<div class="container">
+						<div class="columns is-vcentered is-mobile">
+							<div class="column is-8">
+								<h2 class="mt-2 mb-4 is-size-1 has-text-weight-bold">
+									Hot topics
+								</h2>
+							</div>
+							<div class="column has-text-right">
+								<a class="button is-rounded is-outlined" href="/topics/">
+									More...
+								</a>
+							</div>
+						</div>
+						{/* TODO PWE: Copied from TopicsLayout.11ty.tsx */}
+						<div class="columns is-multiline">
+							{topicsForNewestResources.map((topic) => {
+								let figure: string;
+								if (topic.icon) {
+									figure = (
+										<i class={`${topic.icon} has-text-${topic.accent} fa-5x`} />
+									);
+								} else if (topic.logo) {
+									figure = <img src={topic.logo} alt={topic.title} />;
+								} else {
+									figure = (
+										<i class={`fas fa-file has-text-${topic.accent} fa-5x`} />
+									);
+								}
+
+								return (
+									<div class="column is-6 is-4-desktop mb-5 has-box-hover">
+										<div class="is-flex has-position-relative">
+											<span class="mr-4">
+												<a href={topic.url}>
+													<figure class="image is-128x128 has-text-centered">
+														{figure}
+													</figure>
+												</a>
+											</span>
+											<div>
+												<a
+													href={topic.url}
+													aria-label={`Topic`}
+													class="is-size-5 has-text-weight-bold mb-2 title is-stretched-link"
+												>
+													{topic.title}
+												</a>
+												{topic.subtitle && (
+													<p class="has-text-grey-dark">{topic.subtitle}</p>
+												)}
+											</div>
+										</div>
+									</div>
+								);
+							})}
+						</div>
+					</div>
+				</section>
 
 				<MultiColumnSection>
 					<div>
