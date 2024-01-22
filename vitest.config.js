c8f360e194b07be0f28b70f287e13e7fadf3836a
pauleveritt <paul.everitt@jetbrains.com>
2024-01-22 16:58:12 -0500

    Add esbuild setup to vitest config.

--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+	esbuild: {
+		jsx: "transform",
+		jsxInject: "import { jsx } from 'jsx-async-runtime/jsx-runtime'",
+		jsxFactory: "jsx",
+		jsxImportSource: "jsx-async-runtime",
+	},
 	test: {
 		setupFiles: ["./setup.vitest.ts"],
 		environment: "happy-dom",
