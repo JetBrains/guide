db526ca4348999dbf27e1530ae414a14826c485a
Maarten Balliauw <maarten@maartenballiauw.be>
2024-02-09 13:45:04 +0100

    Potential fix

--- _includes/userComments.11ty.tsx
+++ _includes/userComments.11ty.tsx
@@ -1,3 +1,5 @@
+import { Fragment } from "jsx-async-runtime/jsx-dev-runtime";
+
 export type UserCommentsProps = {
 	pageUrl: string;
 	theme: "light" | "dark";
@@ -19,11 +21,11 @@ export const UserComments = ({
 	const remark_script = `!function(e,n){for(var o=0;o<e.length;o++){var r=n.createElement("script"),c=".js",d=n.head||n.body;"noModule"in r?(r.type="module",c=".mjs"):r.async=!0,r.defer=!0,r.src=remark_config.host+"/web/"+e[o]+c,d.appendChild(r)}}(remark_config.components||["embed"],document);`;
 
 	return (
-		<>
+		<Fragment>
 			<hr />
 			<div id="remark42"></div>
 			<script>{remark_config}</script>
 			<script>{remark_script}</script>
-		</>
+		</Fragment>
 	);
 };
