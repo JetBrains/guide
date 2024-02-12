ce68a7247f0950d385d88fbc047026632caf8031
Maarten Balliauw <maarten@maartenballiauw.be>
2024-02-12 07:17:44 +0100

    Add namecheap.com as domain to exclude for link checking

--- src/scanner.js
+++ src/scanner.js
@@ -36,6 +36,7 @@ const siteChecker = new SiteChecker(
 			"mvnrepository",
 			"mysql.com",
 			"irina.codes",
+			"namecheap.com",
 		],
 		userAgent:
 			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
