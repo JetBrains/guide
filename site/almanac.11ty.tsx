b7e22b92e38577f6346872b036a4d540483bebba
Jan-Niklas Wortmann <jan-niklas.wortmann@jetbrains.com>
2024-07-03 14:56:53 -0500

    add statistic calculations

--- site/almanac.11ty.tsx
+++ site/almanac.11ty.tsx
@@ -1,8 +1,16 @@
 import { LayoutContext, LayoutProps } from "../src/models";
 import { BaseLayout } from "../_includes/layouts/BaseLayout.11ty";
-import { ResourceFrontmatter } from "../src/ResourceModels";
+import { Resource, ResourceFrontmatter } from "../src/ResourceModels";
+import { RESOURCE_TYPES } from "../src/resourceType";
 
 type AlmanacProps = LayoutProps & ResourceFrontmatter;
+
+type Row = {
+	byResourceType: Record<RESOURCE_TYPES, number>;
+	byAuthor: Record<string, number>;
+};
+
+type Data = Record<string, Row>;
 
 export default class AlmanacPage {
 	data() {
@@ -14,10 +22,52 @@ export default class AlmanacPage {
 	}
 
 	render(this: LayoutContext, data: AlmanacProps): JSX.Element {
-		const resources = this.getResources({
-			// resourceTypes: ["tip", "tutorial", "playlist"],
-			// limit: 12,
-		});
+		const resources = this.getResources() as Resource[];
+		const resourceTypes: Record<string, true> = {} as any;
+		const aggregatedData = resources.reduce((acc, resource) => {
+			const res = resource as Resource;
+			// if (res.resourceType === "author") {
+			// 	return { ...acc };
+			// }
+			resourceTypes[resource.resourceType] = true;
+			const date = `${res.date.getFullYear()}-${res.date.getMonth() + 1}`;
+			const t = {
+				...acc,
+				[date]: {
+					byResourceType: {
+						...(acc[date]?.byResourceType ?? {}),
+						[resource.resourceType]:
+							(acc[date]?.byResourceType[resource.resourceType] ?? 0) + 1,
+					},
+					byAuthor: {
+						...(acc[date]?.byAuthor ?? {}),
+						[resource.references?.author?.title ?? resource.author]:
+							(acc[date]?.byAuthor[
+								resource.references?.author?.title ?? resource.author
+							] ?? 0) + 1,
+					},
+				},
+			};
+			return t;
+		}, {} as Data);
+
+		const calculateSum = (data: Record<string, number>) =>
+			Object.values(data).reduce((acc, curr) => {
+				return acc + curr;
+			}, 0);
+
+		function sortAuthorsByAmount(byAuthor: Record<string, number>): Array<{
+			author: string;
+			amount: number;
+		}> {
+			const res = Object.entries(byAuthor).reduce(
+				(acc, [key, value]) => {
+					return [...acc, { author: key, amount: value }];
+				},
+				[] as { author: string; amount: number }[],
+			);
+			return res.sort((a, b) => b.amount - a.amount);
+		}
 
 		return (
 			<BaseLayout {...data}>
@@ -29,42 +79,39 @@ export default class AlmanacPage {
 						<table class="table">
 							<thead>
 								<tr>
-									<th>Month</th>
+									<th style={{ width: "90px" }}>Month</th>
 									<th>All</th>
-									<th>Tips</th>
-									<th>Tutorials</th>
-									<th>Tutorial Steps</th>
-									<th>Links</th>
-									<th>Articles</th>
+									{Object.keys(resourceTypes).map((resourceType) => (
+										<th>{resourceType}</th>
+									))}
 									<th>Authors</th>
 								</tr>
 							</thead>
 							<tbody>
-								<tr>
-									<td>2024-07</td>
-									<td>67</td>
-									<td>41</td>
-									<td>13</td>
-									<td>2</td>
-									<td>3</td>
-									<td>4</td>
-									<td>
-										<span>Helen Scott (22)</span>,<span>Paul Everitt (11)</span>
-										,
-									</td>
-								</tr>
-								<tr>
-									<td>2024-06</td>
-									<td>33</td>
-									<td>11</td>
-									<td>13</td>
-									<td>2</td>
-									<td>3</td>
-									<td>4</td>
-									<td>
-										<span>Helen Scott (22)</span>,
-									</td>
-								</tr>
+								{Object.keys(aggregatedData).map((date) => (
+									<tr>
+										<td>{date}</td>
+										<td class="has-text-right">
+											{calculateSum(aggregatedData[date].byResourceType)}
+										</td>
+										{Object.keys(resourceTypes).map((resourceType) => (
+											<td class="has-text-right">
+												{aggregatedData[date].byResourceType[
+													resourceType as RESOURCE_TYPES
+												] ?? "0"}
+											</td>
+										))}
+										<td>
+											{sortAuthorsByAmount(aggregatedData[date].byAuthor).map(
+												({ author, amount }) => (
+													<span>
+														{author} ({amount}),
+													</span>
+												),
+											)}
+										</td>
+									</tr>
+								))}
 							</tbody>
 						</table>
 					</div>
