40d6a00997235a9cf09a0d0a0e8eb94216d311cc
Maarten Balliauw <maarten@maartenballiauw.be>
2024-09-19 10:44:47 +0200

    Refactor PromoBanner to simplify conditional rendering
    This commit changes the PromoBanner component to use an if-else structure instead of multiple conditionally rendered fragments. This makes the code more straightforward and easier to maintain, reducing the chance of errors. The component now falls back to an empty string if no conditions are met.

--- _includes/navbar/PromoBanner.11ty.tsx
+++ _includes/navbar/PromoBanner.11ty.tsx
@@ -4,64 +4,72 @@ export type PromoBannerProps = {
 	channel: Channel;
 };
 const PromoBanner = ({ channel }: PromoBannerProps): JSX.Element => {
-	return (
-		<>
-			{channel.slug == "dotnet" && new Date() <= new Date(2024, 9, 26) && (
-				<section class="hero is-primary has-text-white">
-					<div class="container hero-body p-2 is-align-content-center level">
-						<div class="level-item mr-4">
-							Join us for a free 2-day community event &ndash; JetBrains .NET
-							Days Online 2024
-						</div>
-						<div class="level-item">
-							<a
-								class="button is-rounded is-white"
-								href="https://lp.jetbrains.com/dotnet-days-2024/?utm_source=google&utm_medium=referral&utm_campaign=guide&utm_content=guide"
-							>
-								Register now!
-							</a>
-						</div>
+	if (channel.slug == "dotnet" && new Date() <= new Date(2024, 9, 26)) {
+		return (
+			<section class="hero is-primary has-text-white">
+				<div class="container hero-body p-2 is-align-content-center level">
+					<div class="level-item mr-4">
+						Join us for a free 2-day community event &ndash; JetBrains .NET Days
+						Online 2024
 					</div>
-				</section>
-			)}
-			{channel.slug == "javascript" && new Date() <= new Date(2024, 10, 24) && (
-				<section class="hero is-primary has-text-white">
-					<div class="container hero-body p-2 is-align-content-center level">
-						<div class="level-item mr-4">
-							Join us for a free community event &ndash; JetBrains JavaScript
-							Day 2024
-						</div>
-						<div class="level-item">
-							<a
-								class="button is-rounded is-white"
-								href="https://lp.jetbrains.com/javascript-day-2024/?utm_source=google&utm_medium=referral&utm_campaign=guide&utm_content=guide"
-							>
-								Register now!
-							</a>
-						</div>
+					<div class="level-item">
+						<a
+							class="button is-rounded is-white"
+							href="https://lp.jetbrains.com/dotnet-days-2024/?utm_source=google&utm_medium=referral&utm_campaign=guide&utm_content=guide"
+						>
+							Register now!
+						</a>
 					</div>
-				</section>
-			)}
-			{channel.slug == "gamedev" && new Date() <= new Date(2024, 10, 10) && (
-				<section class="hero is-primary has-text-white">
-					<div class="container hero-body p-2 is-align-content-center level">
-						<div class="level-item mr-4">
-							Join us for a free community event &ndash; JetBrains GameDev Days
-							2024
-						</div>
-						<div class="level-item">
-							<a
-								class="button is-rounded is-white"
-								href="https://lp.jetbrains.com/gamedev-days-2024/?utm_source=google&utm_medium=referral&utm_campaign=guide&utm_content=guide"
-							>
-								Register now!
-							</a>
-						</div>
+				</div>
+			</section>
+		);
+	} else if (
+		channel.slug == "javascript" &&
+		new Date() <= new Date(2024, 10, 24)
+	) {
+		return (
+			<section class="hero is-primary has-text-white">
+				<div class="container hero-body p-2 is-align-content-center level">
+					<div class="level-item mr-4">
+						Join us for a free community event &ndash; JetBrains JavaScript Day
+						2024
 					</div>
-				</section>
-			)}
-		</>
-	);
+					<div class="level-item">
+						<a
+							class="button is-rounded is-white"
+							href="https://lp.jetbrains.com/javascript-day-2024/?utm_source=google&utm_medium=referral&utm_campaign=guide&utm_content=guide"
+						>
+							Register now!
+						</a>
+					</div>
+				</div>
+			</section>
+		);
+	} else if (
+		channel.slug == "gamedev" &&
+		new Date() <= new Date(2024, 10, 10)
+	) {
+		return (
+			<section class="hero is-primary has-text-white">
+				<div class="container hero-body p-2 is-align-content-center level">
+					<div class="level-item mr-4">
+						Join us for a free community event &ndash; JetBrains GameDev Days
+						2024
+					</div>
+					<div class="level-item">
+						<a
+							class="button is-rounded is-white"
+							href="https://lp.jetbrains.com/gamedev-days-2024/?utm_source=google&utm_medium=referral&utm_campaign=guide&utm_content=guide"
+						>
+							Register now!
+						</a>
+					</div>
+				</div>
+			</section>
+		);
+	} else {
+		return "";
+	}
 };
 
 export default PromoBanner;
