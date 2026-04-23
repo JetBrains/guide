70f91a557b943c02c903863bd9455acfc541b581
pauleveritt <pauleveritt@me.com>
2026-04-23 06:56:59 -0400

    Add `show` prop to InfoBanner and render null when false

--- _includes/navbar/InfoBanner.11ty.tsx
+++ _includes/navbar/InfoBanner.11ty.tsx
@@ -1,8 +1,13 @@
 export type InfoBannerProps = {
 	children?: JSX.Children;
+	show?: boolean;
 };
 
-const InfoBanner = ({ children }: InfoBannerProps): JSX.Element => {
+const InfoBanner = ({
+	children,
+	show = false,
+}: InfoBannerProps): JSX.Element | null => {
+	if (!show) return null;
 	return (
 		<section class="has-background-info has-text-white py-2 has-text-centered">
 			<span class="icon-text">
